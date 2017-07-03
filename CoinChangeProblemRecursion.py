ee45202748464d6e3d7f5aa1dc8392a856b3b2e3
Pradeep K. Pant <ppant@cpan.org>
2017-07-03 15:27:28 +0530

    Coin change problem recursion

---
+++ CoinChangeProblemRecursion.py
@@ -0,0 +1,46 @@
+# Implement coin change problem
+# Author: Pradeep K. Pant, ppant@cpan.org
+# Given a target amount n and a list (array) of distinct coin values, what's the fewest coins needed to make the
+# change amount.
+# 1+1+1+1+1+1+1+1+1+1
+# 5 + 1+1+1+1+1
+# 5+5
+# 10
+# With 1 coin being the minimum amount.
+
+# Solution strategy:
+# This is a classic problem to show the value of dynamic programming. We'll show a basic recursive example and show
+# why it's actually not the best way to solve this problem.
+# Implement fibonacci_iterative()
+def coin_change_recursion(target,coins):
+    '''
+        INPUT: Target change amount and list of coin values
+        OUTPUT: Minimum coins needed to make change
+
+        Note, this solution is not optimized.
+        '''
+
+    # Default to target value
+    min_coins = target
+
+    # Check to see if we have a single coin match (BASE CASE)
+    if target in coins:
+        return 1
+
+    else:
+
+        # for every coin value that is <= than target
+        for i in [c for c in coins if c <= target]:
+
+            # Recursive Call (add a count coin and subtract from the target)
+            num_coins = 1 + coin_change_recursion(target - i, coins)
+
+            # Reset Minimum if we have a new minimum
+            if num_coins < min_coins:
+                min_coins = num_coins
+
+    return min_coins
+
+# Test
+print (coin_change_recursion(8,[1,5]))
+# 2
