542277ebea0fdef59ab4bfa2cff6171cd11a65e4
Pradeep K. Pant <ppant@cpan.org>
2017-07-19 18:26:25 +0530

    Tree representation imple

---
+++ TreeRepresentationWithNodesReferences.py
@@ -0,0 +1,62 @@
+# Nodes and References Implementation of a Tree
+
+# Author: Pradeep K. Pant, ppant@cpan.org
+# implementing the representation of a Tree as a class with nodes and references!
+
+class BinaryTree(object):
+    def __init__(self,rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+    def insertLeft(self,newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self,newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self,obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
+
+
+# Test
+r = BinaryTree('a')
+print(r.getRootVal())
+print(r.getLeftChild())
+r.insertLeft('b')
+print(r.getLeftChild())
+print(r.getLeftChild().getRootVal())
+r.insertRight('c')
+print(r.getRightChild())
+print(r.getRightChild().getRootVal())
+r.getRightChild().setRootVal('hello')
+print(r.getRightChild().getRootVal())
+
+#  Output
+#a
+#None
+#<__main__.BinaryTree object at 0x104779c10>
+#b
+#<__main__.BinaryTree object at 0x103b42c50>
+#c
+#hello
