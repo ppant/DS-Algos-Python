3e081a97fb52cb915fcec931ad12938de9828778
Pradeep K. Pant <ppant@cpan.org>
2017-07-25 18:02:29 +0530

    Selection sort imple updated

--- SelectionSortImple.py
+++ SelectionSortImple.py
@@ -1,15 +1,25 @@
 # Selection Sort Implementation
 
 # Author: Pradeep K. Pant, ppant@cpan.org
-# The selection sort improves on the bubble sort by making only 
-# one exchange for every pass through the list. In order to do this, 
-# a selection sort looks for the largest value as it makes a pass 
-# and, after completing the pass, places it in the proper location. 
-# As with a bubble sort, after the first pass, the largest item is 
-# in the correct place. After the second pass, the next largest is 
-# in place. This process continues and requires n−1 passes to sort 
-# n items, since the final item must be in place after the (n−1) st pass.
 
+#	* Selection sort is a in-place algorithm 
+#	* It works well with small files
+#	* It is used for sorting the files with large values and small keys this is 
+#   due to the fact that selection is based on keys and swaps are made only when required
+#	* The selection sort improves on the bubble sort by making only 
+#   one exchange for every pass through the list. In order to do 
+#   this, a selection sort looks for the largest value as it makes 
+#   a pass and, after completing the pass, places it in the proper 
+#   location. As with a bubble sort, after the first pass, the largest 
+#   item is in the correct place. After the second pass, the next 
+#   largest is in place. This process continues and requires n−1 
+#   passes to sort n items, since the final item must be in place 
+#   after the (n−1) st pass
+	
+# * Performance: 
+#	* 
+#		* worst case: O(n2) n square, best case O(n), average case: O(n2) n square, worst case space complexity: O(1)
+# Ref: http://interactivepython.org/runestone/static/pythonds/SortSearch/TheSelectionSort.html
 def selection_sort(arr):
     
     # For every slot in array
