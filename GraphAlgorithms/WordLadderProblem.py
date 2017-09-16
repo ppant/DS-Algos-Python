7d1a4aea78e8e0f9c799fa8553b86a892f4b2437
Pradeep K. Pant <ppant@cpan.org>
2017-09-16 19:27:48 +0530

    Initial skeleton and documentaion

---
+++ GraphAlgorithms/WordLadderProblem.py
@@ -0,0 +1,24 @@
+# Graph Word ladder problem
+
+# Author: Pradeep K. Pant, ppant@cpan.org
+# Ref: http://interactivepython.org/runestone/static/pythonds/Graphs/BuildingtheWordLadderGraph.html
+
+# let’s consider the following puzzle called a word ladder. Transform the 
+# word “FOOL” into the word “SAGE”. In a word ladder puzzle you must make 
+# the change occur gradually by changing one letter at a time. At each step 
+# you must transform one word into another word, you are not allowed to 
+# transform a word into a non-word. 
+# The following sequence of words shows one possible solution to the problem posed above.
+#FOOL
+#POOL
+#POLL
+#POLE
+#PALE
+#SALE
+#SAGE
+# There are many variations of the word ladder puzzle. For example you might 
+# be given a particular number of steps in which to accomplish the transformation, 
+# or you might need to use a particular word. In this section we are interested in 
+# figuring out the smallest number of transformations needed to turn the starting word 
+# into the ending word.
+
