a2de5d6c70bdd8352592452d3dd5a7cfc9ffce32
Pradeep K. Pant <pantpradeep@gmail.com>
2021-06-29 14:30:58 +0530

    Update BubbleSortImple.py

--- Sorting/BubbleSortImple.py
+++ Sorting/BubbleSortImple.py
@@ -1,6 +1,6 @@
 # Bubble Sort Implementation
 
-# Author: Pradeep K. Pant, ppant@cpan.org
+# Author: Pradeep K. Pant, ppant@cpan.org,  https://pradeeppant.com
 # The bubble sort makes multiple passes through a list. It compares
 #  adjacent items and exchanges those that are out of order. 
 # Each pass through the list places the next largest value in its 
@@ -9,18 +9,18 @@
 
 
 # * Regardless of how the items are arranged in the initial list, n−1 passes will be 
-# made to sort a list of size n, so 1 pass n-1 comparisons, 2 pass n-2 comparions and n-1 is 1 comparions.
+# made to sort a list of size n, so 1 pass is n-1 comparisons, 2 pass is n-2 comparions and n-1 is 1 comparions.
 # * A bubble sort is often considered the most inefficient sorting method since it 
 # must exchange items before the final location is known. These “wasted” exchange 
 # operations are very costly. However, because the bubble sort makes passes through 
 # the entire unsorted portion of the list, it has the capability to do something most 
 # sorting algorithms cannot. In particular, if during a pass there are no exchanges, 
 # then we know that the list must be sorted. A bubble sort can be modified to stop early 
-# if it finds that the list has become sorted. This means that for lists that require just 
+# , if it finds that the list has already become sorted. This means that for lists that require just 
 # a few passes, a bubble sort may have an advantage in that it will recognize the sorted list and stop.
 # * Performance: Worst case: O(n2) n square, best case O(n), average case: O(n2) n square
 
-# Ref: http://interactivepython.org/runestone/static/pythonds/SortSearch/TheBubbleSort.html
+# Reference material: http://interactivepython.org/runestone/static/pythonds/SortSearch/TheBubbleSort.html
 
 
 def bubble_sort(arr):
